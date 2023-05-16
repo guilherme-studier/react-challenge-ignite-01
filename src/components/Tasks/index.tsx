4d89d55a5920ffab327ec897261b8084bd079127
Guilherme <guilherme.studier18@gmail.com>
2023-05-15 21:46:59 -0300

    first commit

--- src/components/Tasks/index.tsx
+++ src/components/Tasks/index.tsx
@@ -37,7 +37,7 @@ export function Tasks({ tasks, onDelete, onComplete }: Props) {
           <section className={styles.empty}>
             <TbClipboardText size={50} />
             <div>
-              <p>Você não tem tarefas cadastradas</p>
+              <p>Você ainda não tem tarefas cadastradas</p>
               <span>Crie tarefas e organize seus itens a fazer</span>
             </div>
           </section>
